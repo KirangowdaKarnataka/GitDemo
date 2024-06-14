904c0016da2c4f1cafead27bb7a1bce9bd131cfb
kirangowda <kirangowda456qwe@gmail.com>
2024-06-14 20:11:00 +0530

    Austrlian guy commit

--- src/main/java/org/selenium4/latest/version/check/TestDemo.java
+++ src/main/java/org/selenium4/latest/version/check/TestDemo.java
@@ -5,7 +5,12 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Heloow World");
-		System.out.println("Good morning");
+		System.out.println("Good morning y");
+		System.out.println("Good morning o");
+		System.out.println("hello world");
+		
+		
+		System.out.println("---------------------------------------------------");
 		
 		
 
---
+++ src/main/java/org/selenium4/latest/version/check/TestingGit.java
@@ -0,0 +1,12 @@
+package org.selenium4.latest.version.check;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Good to go");
+
+	}
+
+}
