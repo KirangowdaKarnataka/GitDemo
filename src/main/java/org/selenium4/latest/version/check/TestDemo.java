74e20c62176459f6ab6265e26387d041f708c2a6
kirangowda <kirangowda456qwe@gmail.com>
2024-06-14 19:52:39 +0530

    this is commit from Asian time zone

---
+++ src/main/java/org/selenium4/latest/version/check/TestDemo.java
@@ -0,0 +1,11 @@
+package org.selenium4.latest.version.check;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heloow World");
+
+	}
+
+}
